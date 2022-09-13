0558967d07023e15fa2391ac7d2cd644db14859f
moraesclei@gmail.com <moraesclei@gmail.com>
2022-09-13 19:22:29 -0300

    first commit

---
+++ hello-world/src/LacosNumericos.java
@@ -0,0 +1,19 @@
+public class LacosNumericos {
+    
+    public static void main(String[] args) {
+        // 1 2 3 4 5 6 7 8 9 10
+        //para uma variavel que inicia em 1, e vai ate 10, mudando 1-por-1, faça:
+        //for (int i = 1; i <= 10; i+=2){
+        //    System.out.println(i);
+        //}
+        for (int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                System.out.println(j + " x " + i + " = " + j * i); 
+                //1x1 = 1
+                //2x1 = 2   
+            }
+        }
+
+    }
+
+}
---
+++ hello-world/src/VetoresArrays.java
@@ -0,0 +1,25 @@
+public class VetoresArrays {
+    
+    public static void main(String[] args) {
+        /* 
+        int[] numeros = new int[5];
+        //[0] [1] [2] [3] [4] 
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+        for (int i=0; i < numeros.length; i++){
+            System.out.println(numeros[i]);
+        }*/
+        String[] letras = new String[5];
+        letras[0]="A";
+        letras[1]="B";
+        letras[2]="C";
+        letras[3]="J";
+        letras[4]="x";
+        for(int i=0; i < letras.length; i++){
+            System.out.println(letras[i]);
+        }
+    }
+}
